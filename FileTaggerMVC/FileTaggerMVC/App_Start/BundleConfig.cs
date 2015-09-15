ad6bd56b93b89afc72bbdcbd41ef7d313e78d942
Bavine Aracchande <bavine0@gmail.com>
2015-09-15 20:17:54 +0100

    Initial commit

---
+++ FileTaggerMVC/FileTaggerMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Optimization;
+
+namespace FileTaggerMVC
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.11.3.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jstree").Include(
+                      "~/Scripts/jstree.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/jstree/default/style.min.css",
+                      "~/Content/jstree/default-dark/style.min.css"));
+        }
+    }
+}
