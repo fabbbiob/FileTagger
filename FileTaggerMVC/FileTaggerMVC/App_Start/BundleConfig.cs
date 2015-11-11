95a0fd2333aacbe97a1f93e97a3457b5ba25552d
baracchande <bavine0@gmail.com>
2015-11-11 13:43:27 +0000

    bundles

--- FileTaggerMVC/FileTaggerMVC/App_Start/BundleConfig.cs
+++ FileTaggerMVC/FileTaggerMVC/App_Start/BundleConfig.cs
@@ -7,20 +7,12 @@ namespace FileTaggerMVC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-1.11.3.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jquery.validate").Include(
-                        "~/Scripts/jquery.validate.min.js", 
-                        "~/Scripts/jquery.validate.unobtrusive.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jstree").Include(
-                      "~/Scripts/jstree.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/selectize").Include(
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/jquery-1.11.3.min.js",
+                "~/Scripts/jquery.validate.min.js",
+                "~/Scripts/jquery.validate.unobtrusive.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/jstree.min.js",
                 "~/Scripts/selectize.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
